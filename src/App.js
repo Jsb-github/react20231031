9e10709e0a5900e9335b03f4d9c68149e95663a6
jsb <tjdqls2021@gmail.com>
2023-10-31 12:14:45 +0900

    Component,연습, 작성완료

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+        {/*<h1>Hello Component </h1>*/}
+      <MyComponent2 />
+        {/*<p> lorem ipsum </p>
+            <ul>
+              <li><li/>
+              <li><li/>
+              <li><li/>
+        */}
     </div>
   );
+}
+function MyComponent1(){
+    return <h1>Hello Componet</h1>
+}
+
+function MyComponent2(){
+    return(
+        <>
+            {/*p>lorem5^ul>li*3>lorem1 */}
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem ipsum dolor.</li>
+                <li>Magni, praesentium, recusandae.</li>
+                <li>Asperiores, fuga, porro.</li>
+            </ul>
+        </>
+
+    )
 }
 
 export default App;
