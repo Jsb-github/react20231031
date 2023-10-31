c4965c535d8887d9e626def12f6d04dfce4f794e
jsb <tjdqls2021@gmail.com>
2023-10-31 15:21:02 +0900

    JSX의 {}에서 간단한 연산자 활용

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
-function MyComp2() {
-  let name = "손흥민";
-  let age = 33;
-
-  // {} jsx에서 js 코드 작성하기 위한 기호
-  return (
-    <>
-      <h1>hello React</h1>
-      <h1>hello {name}</h1>
-      <h1>{age} years</h1>
-      <h1>{age * 2} double years</h1>
-      <h1>{name + "선수"}</h1>
-    </>
-  );
-}
-
 function App() {
-  // jsx에서 태그 사용시 꼭 종료태그를 작성해야 함
-  // 빈 요소이면 시작태그에서 종료
-  return (
-    <div>
-      <h1>hello react</h1>
-      <h2>HI REACT</h2>
-      <p>
-        Lorem.
-        <br />
-        Lorem ipsum dolor.
-      </p>
-      <MyComp />
-      <MyComp2 />
-    </div>
-  );
-}
-
-// built-in component는 소문자로 시작
-// component 명은 꼭 대문자로 시작
-// return 문을 가져야함
-// return 값은 보통 JSX 코드
-function MyComp() {
-  // return 에 jsx 코드 작성 가능
-  // 꼭 하나의 root tag가 존재 해야함
-  // return 값이 여러 줄이면 ()로 감싸기
-  // root tag가 필요 없다면 fragment => <> 사용 가능
-
-  // jsx에서 주석 사용법
-  // {/* */}
+  // 1~100
+  const number = Math.ceil(Math.random() * 100);
+  const isLarge = number > 50;
+  const isSmall = number <= 50;
 
   return (
     <>
-      {" "}
-      {/*<> => fragment*/}
-      <h1>HI COMPONENT</h1>
-      <h2>hello</h2>
+      <h1>난수 : {number}</h1>
+      <h1> {number > 50 ? "큰수" : "작은수"}</h1>
+      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
+      <div>{isLarge && <h1>큰 수다!!</h1>}</div>
+      <div>{isLarge || <h1>작은 수다</h1>}</div>
     </>
   );
 }
 
+function BigImage() {
+  return <h1>큰 이미지</h1>;
+}
+
+function SmallImage() {
+  return <h1>작은 이미지</h1>;
+}
 export default App;
