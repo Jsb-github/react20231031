1e0b24b25bae0aa7f3eac7f38cb419c69a83f6e7
jsb <tjdqls2021@gmail.com>
2023-11-01 12:22:11 +0900

    한 파일에서 여러개의 값 (객체, 값 함수 ) export import가능

--- src/component/MyElem.js
+++
@@ -1,9 +0,0 @@
-// 다른 파일에서 사용 가능하도록 export
-
-export function MyElem() {
-  return (
-    <div>
-      <p>매우매우 긴 코드</p>
-    </div>
-  );
-}
--- src/component/MyHeader.js
+++
@@ -1,7 +0,0 @@
-export function MyHeader() {
-  return (
-    <div>
-      <h1>너무 길어짐</h1>
-    </div>
-  );
-}
