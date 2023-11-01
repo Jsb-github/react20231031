fbb4214e3d6c8366f847747d71868f87e2066225
jsb <tjdqls2021@gmail.com>
2023-11-01 15:27:20 +0900

    보통의 css 파일 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import MyBox, { address } from "./component/MyBox";
-import MyElem, { address as myAddress } from "./component/MyElem";
+// style 적용 방법
+// 보통 css 파일
+// css module 사용
+// chakraui css library
+import { MyComp } from "./component/MyComp";
+import { MyBox } from "./component/MyBox";
 
-// as로 named export 값을 import할 때 별칭을 줄 수 있음
 function App() {
   return (
     <>
-      <h1>{address}</h1>
-      <h1>{myAddress}</h1>
-      <MyElem />
+      <MyComp />
       <MyBox />
     </>
   );
