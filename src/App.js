ed92f638d215f0f632b99827b26b95274b15d6ca
jsb <tjdqls2021@gmail.com>
2023-11-02 15:15:29 +0900

    Select 사용

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
-import {
-  Box,
-  Button,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
-import { EmailIcon, NotAllowedIcon } from "@chakra-ui/icons";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Center>
-        <Box w="480px">
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>띄어쓰기 없이 입력하세요</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt="30px">
-        <Box w="480px">
-          <FormControl>
-            <FormLabel>하나만 선택하세요</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Id.</Radio>
-                <Radio value="value4">Aliquid.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-
-      <Center>
-        <Box w="480px">
-          <FormControl>
-            <CheckboxGroup>
-              <Flex>
-                <Checkbox colorScheme="blue">lorem</Checkbox>
-                <Checkbox colorScheme="facebook">Lorem ipsum.</Checkbox>
-                <Checkbox colorScheme="teal">Ducimus, quo!</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w="480px">
+        <FormControl>
+          <Select placeholder="선택하세요." multiple h="120px">
+            <option value="value1">Lorem.</option>
+            <option value="value2">Vitae?</option>
+            <option value="value3">Velit.</option>
+            <option value="value4">Deleniti.</option>
+            <option value="value5">Adipisci?</option>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 }
