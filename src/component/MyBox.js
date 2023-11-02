a49adc47e338541f0fbee0c6de6c96f4ef95eb0c
jsb <tjdqls2021@gmail.com>
2023-11-02 09:34:34 +0900

    필요없는 파일들 삭제함

--- src/component/MyBox.js
+++
@@ -1,9 +0,0 @@
-import style from "../style/style1.module.css";
-
-export function MyBox() {
-  return (
-    <div>
-      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}
