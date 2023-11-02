ff534cc23cf5f0096e096c93928a27780d1325e8
jsb <tjdqls2021@gmail.com>
2023-11-02 10:23:39 +0900

    필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
-  return (
-    <>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="red">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue">Cupiditate facere, repellat?</Box>
-      <Box bgColor="blue.50">Architecto incidunt, repellendus?</Box>
-      <Box bgColor="blue.100">Cupiditate, ratione, voluptas!</Box>
-      <Box bgColor="blue.900">Delectus, doloremque modi.</Box>
-      <Box bgColor="gray">Consectetur, molestiae reiciendis?</Box>
-      <Box bgColor="gray.100">Est illum, nesciunt!</Box>
-      <Box bgColor="gray.800">Officiis, quis, quo.</Box>
-      <Box bgColor="yellow.600" color="gray.200">
-        Cupiditate, ex temporibus.
-      </Box>
-      <Box bg="facebook.500" color="linkedin.100">
-        Impedit tempore, vel.
-      </Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
