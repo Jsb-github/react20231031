9be873d4a35ab1fd570a1d0911b1d323075aa0bf
jsb <tjdqls2021@gmail.com>
2023-11-02 14:34:23 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { EmailIcon, NotAllowedIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            status: "success",
-            duration: 2000,
-            isClosable: true,
-            position: "top",
-          })
-        }
-      >
-        토스트 뛰우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Maiores?</Button>
+      <Button colorScheme="yellow">Iste?</Button>
+      <Button colorScheme="orange">Saepe!</Button>
+      <Button size={"sm"} colorScheme="teal">
+        Quasi.
       </Button>
-
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패",
-            description: "게시물이 저장되지 않아습니다..",
-            status: "error",
-            duration: 2000,
-            isClosable: true,
-            position: "top",
-          })
-        }
-      >
-        토스트 뛰우기
+      <Button colorScheme="messenger" size={"lg"}>
+        Neque!
       </Button>
+      <Button colorScheme="telegram" variant="outline">
+        Voluptate!
+      </Button>
+      <Button colorScheme="orange" variant="ghost">
+        Alias?
+      </Button>
+      <Button variant="link">Nulla.</Button>
+      <Button leftIcon={<EmailIcon />}>Ratione.</Button>
+      <Button rightIcon={<NotAllowedIcon />}>Ratione.</Button>
+      <Button isLoading>Quasi.</Button>
+      <Button isLoading loadingText={"전송중"}>
+        Ipsam.
+      </Button>
+      <Button>Molestiae.</Button>
+      <Button>Architecto!</Button>
+      <Button>Dolor!</Button>
+      <Button>Obcaecati.</Button>
+      <Button>Quia.</Button>
+      <Button>Molestias!</Button>
+      <Button>Maxime.</Button>
+      <Button>Repellat.</Button>
+      <Button>Provident!</Button>
+      <Button>Est!</Button>
+      <Button>Quos.</Button>
+      <Button>Illum?</Button>
+      <Button>Delectus.</Button>
+      <Button>Similique.</Button>
+      <Button>Beatae!</Button>
+      <Button>Iste.</Button>
+      <Button>Nisi.</Button>
     </>
   );
 }
