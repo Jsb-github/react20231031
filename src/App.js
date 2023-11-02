ed74cfe566838fb945dd90631e8e8241bdfdc207
jsb <tjdqls2021@gmail.com>
2023-11-02 12:37:16 +0900

    Center, Square, Circle 사용

--- src/App.js
+++ src/App.js
@@ -1,104 +1,20 @@
-import {
-  Box,
-  SimpleGrid,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Box, Center, Circle, Square } from "@chakra-ui/react";
+import { DeleteIcon, PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          {/*Table>Thead>Tr>Th*4^^^Tbody>Tr*5>Td*4>lorem1*/}
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>Lorem ipsum dolor.</Th>
-                <Th>Doloremque, illum, quidem!</Th>
-                <Th>Fugiat, soluta, velit.</Th>
-                <Th>Dicta, vero, voluptate!</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Quae.</Td>
-                <Td>Sequi!</Td>
-                <Td>Iusto?</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Repellendus?</Td>
-                <Td>Molestias!</Td>
-                <Td>Fuga.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Sequi!</Td>
-                <Td>Eum?</Td>
-                <Td>Delectus?</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Quidem!</Td>
-                <Td>Officiis?</Td>
-                <Td>Aliquam.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Rem!</Td>
-                <Td>Eius!</Td>
-                <Td>Ipsa?</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
+      <Center bg={"red"} h={"200px"}>
+        <Square bg={"gray"} w={"100px"} h={"100px"}>
+          <DeleteIcon />
+        </Square>
+      </Center>
 
-        {/*Box>Table[variant=striped]>Thead>Tr>Th*4>lorem1^^^Tbody>Tr**/}
-        <Box>
-          <Table variant="striped" colorScheme="red" size="lg">
-            <Thead>
-              <Tr>
-                <Th>Lorem.</Th>
-                <Th>Culpa.</Th>
-                <Th>Quam.</Th>
-                <Th isNumeric>Hic.</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Molestias.</Td>
-                <Td>Animi?</Td>
-                <Td isNumeric>Laudantium.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Molestias.</Td>
-                <Td>Animi?</Td>
-                <Td isNumeric>Laudantium.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Molestias.</Td>
-                <Td>Animi?</Td>
-                <Td isNumeric>Laudantium.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Molestias.</Td>
-                <Td>Animi?</Td>
-                <Td isNumeric>Laudantium.</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-      </SimpleGrid>
+      <Center bg={"red.200"} h={"200px"}>
+        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
+          <PhoneIcon />
+        </Circle>
+      </Center>
     </>
   );
 }
