6a52fdbb35434f22ccd1df35a96255d2f34bd991
jsb <tjdqls2021@gmail.com>
2023-11-07 14:53:13 +0900

    component 밖의 값을 가능 한 변경하지 않도록 할 것!!

--- src/Bcomp.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { Text } from "@chakra-ui/react";
-import { MessageContext } from "./App";
-export function Bcomp() {
-  //3. context 사용하기 : useContext
-  // const value = useContext(MessageContext);
-  const { message } = useContext(MessageContext);
-  return <Text>{message}</Text>;
-}
